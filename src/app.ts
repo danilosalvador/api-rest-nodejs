3215f90aba031563d1b44bf934d8b206d1d9aaba
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2025-02-10 12:00:16 -0300

    Ajustes

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,20 @@ export const app = fastify()
 
 app.register(cookie)
 
+/*
+Hooks no Fastify são funções que podem ser registradas para serem executadas em diferentes momentos do ciclo de vida de uma requisição
+
+- onRequest: Executado antes da autenticação e antes do processamento da requisição.
+- preParsing: Modifica os dados da requisição antes do parsing do corpo.
+- preValidation: Executado antes da validação do payload da requisição.
+- preHandler: Executado antes do handler da rota (útil para autenticação).
+- preSerialization: Modifica a resposta antes de ser serializada.
+- onSend: Modifica a resposta antes de enviá-la ao cliente.
+- onResponse: Executado após a resposta ser enviada.
+- onError: Manipula erros que ocorrem na requisição.
+- onReady: Executado quando o servidor está pronto.
+- onClose: Executado quando o servidor está fechando.
+*/
 app.addHook('preHandler', async request => {
     console.log(`[${request.method}] ${request.url}`)
 })
